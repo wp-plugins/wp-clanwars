30bf222226342fabb82e880fe1269b49a4e68a5e
andddd <andddd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 15:57:20 +0000

    Fix it with sledgehammer
    git-svn-id: https://plugins.svn.wordpress.org/wp-clanwars/trunk@1160577 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 === WP-ClanWars ===
 Contributors: andddd
 Tags: cybersport, clanwar, team, clan, cyber, sport, match
-Requires at least: 3.0.1
-Tested up to: 4.0
-Stable tag: 1.6.3
+Requires at least: 4.1
+Tested up to: 4.2.2
+Stable tag: 1.7.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -69,3 +69,7 @@ Make sure your MySQL server supports InnoDB, otherwise this plugin won't work.
 * Add import/export to ZIP
 * Add "Export" link next to "Maps" in Games section
 * Drop old localizations
+
+== Attributions ==
+
+Plugin icon made by [Freepik](http://www.flaticon.com/authors/freepik "Freepik") from [www.flaticon.com](http://www.flaticon.com "Flaticon") is licensed by [CC BY 3.0](http://creativecommons.org/licenses/by/3.0/ "Creative Commons BY 3.0").
---
+++ js/gallery.js
@@ -0,0 +1,78 @@
+//
+// Admin page: gallery management
+// (c) 2015 Andrej Mihajlov
+//
+
+(function ($, window) {
+
+    function GalleryManager() {
+        GalleryManager.prototype._init.apply(this, arguments);
+    }
+
+    GalleryManager.prototype = {
+
+        _init: function () {
+            var self = this;
+
+            this._container = $('#gallery-container');
+
+            $('#add-gallery-button').on('click', function (e) {
+                var image = wp.media({
+                    title: wpCWL10n.addGallery,
+                    multiple: true
+                })
+                .open()
+                .on('select', function() {
+                    var selection = image.state().get('selection');
+                    var models = selection.where({ type: 'image' });
+                    
+                    $.each(models, function (i, model) {
+                        var atts = model.attributes;
+
+                        self.add(atts.id, atts.url);
+                    });
+                });
+            });
+
+            this._container.sortable({
+                items: '> .gallery-item',
+                placeholder: 'sortable-placeholder',
+                revert: true
+            });
+
+            this._container.on('click', '.gallery-item a.remove', this._onRemove);
+        },
+
+        _onRemove: function (e) {
+            if(confirm(wpCWL10n.confirmDeleteScreenshot)) {
+                $(this).parent().remove();
+            }
+
+            e.preventDefault();
+            e.stopPropagation();
+        },
+
+        add: function (id, url) {
+            var el = $('<div class="gallery-item"><input type="hidden" name="gallery[ids][]" /> \
+                            <a href="#" class="remove"><span class="dashicons dashicons-no"></span></a> \
+                            <div class="centered"><img /></div> \
+                        </div>');
+
+            el.attr('data-id', id)
+                .attr('id', 'gallery-item-' + id)
+                .find('input')
+                    .val(id)
+                .end()
+                .find('img')
+                    .attr('src', url);
+
+            this._container.append(el);
+        }
+
+    };
+
+    $(document).ready(function () {
+        window.wpGalleryManager = new GalleryManager();
+    });
+
+})(jQuery, window);
